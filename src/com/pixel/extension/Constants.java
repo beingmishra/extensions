Nikesh001/packages_apps_TiElements

Bring up TiElements 1.0
Thanks to Vivek Roy for the nice icons

Signed-off-by: Saikat Halder <iamdevsaikat@gmail.com>

src/com/titanium/tielements/Constants.java
@@ -1,0 +1,12 @@
+package com.titanium.tielements;
+
+public interface Constants {
+
+    String MAIN_VIEW = "MAIN_VIEW";
+    String STATUS_BAR_MENU_FRAGMENT = "STATUS_BAR_MENU_FRAGMENT";
+    String NAV_BAR_MENU_FRAGMENT = "NAV_BAR_MENU_FRAGMENT";
+    String QUICK_SETTINGS_MENU_FRAGMENT = "QUICK_SETTINGS_MENU_FRAGMENT";
+    String RECENT_MENU_FRAGMENT = "RECENT_MENU_FRAGMENT";
+    String LOCK_SCREEN_MENU_FRAGMENT = "LOCK_SCREEN_MENU_FRAGMENT";
+    String SYSTEM_MENU_FRAGMENT = "SYSTEM_MENU_FRAGMENT";
+}

src/com/titanium/tielements/fragments/Lockscreen.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2019 TitaniumOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.titanium.tielements.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
+public class Lockscreen extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    
+    private static final String TAG = "Lockscreen";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lockscreen);
+        setRetainInstance(true);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.TIELEMENTS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+}

src/com/titanium/tielements/fragments/Navigation.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2019 TitaniumOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.titanium.tielements.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
+public class Navigation extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    
+    private static final String TAG = "Navigation";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.navigation);
+        setRetainInstance(true);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.TIELEMENTS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+}

src/com/titanium/tielements/fragments/Recents.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2019 TitaniumOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.titanium.tielements.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
+public class Recents extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    
+    private static final String TAG = "Recents";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.recents);
+        setRetainInstance(true);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.TIELEMENTS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+}

src/com/titanium/tielements/fragments/Statusbar.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2019 TitaniumOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.titanium.tielements.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
+public class Statusbar extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    
+    private static final String TAG = "Statusbar";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.statusbar);
+        setRetainInstance(true);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.TIELEMENTS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+}

src/com/titanium/tielements/fragments/System.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2019 TitaniumOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.titanium.tielements.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
+public class System extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    
+    private static final String TAG = "System";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.system);
+        setRetainInstance(true);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.TIELEMENTS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+}

src/com/titanium/tielements/views/MenuViews.java
@@ -1,0 +1,68 @@
+package com.titanium.tielements.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+public class MenuViews extends LinearLayout {
+
+    TextView menu_title;
+    ImageView menu_icon;
+    String title;
+    int icon;
+
+    public MenuViews(Context context) {
+        super(context);
+        init(context, null, 0, 0);
+    }
+
+    public MenuViews(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0, 0);
+    }
+
+    public MenuViews(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr, 0);
+    }
+
+
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        View base = inflate(context, R.layout.menu_view, this);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MenuViews, defStyleAttr, defStyleRes);
+        title = typedArray.getString(R.styleable.MenuViews_title);
+        icon = typedArray.getResourceId(R.styleable.MenuViews_icon, 0);
+        menu_title = base.findViewById(R.id.menu_title);
+        menu_icon = base.findViewById(R.id.menu_icon);
+        if (!title.isEmpty()) {
+            menu_title.setText(title);
+        } else {
+            menu_title.setText("Not Available");
+        }
+        menu_icon.setImageResource(icon);
+        typedArray.recycle();
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setIcon(int image) {
+        this.icon = image;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}

beingmishra/extensions

src/com/pixel/extension/Constants.java
@@ -1,0 +1,12 @@
+package com.pixel.extension;
+
+public interface Constants {
+
+    String MAIN_VIEW = "MAIN_VIEW";
+    String STATUS_BAR_MENU_FRAGMENT = "STATUS_BAR_MENU_FRAGMENT";
+    String NAV_BAR_MENU_FRAGMENT = "NAV_BAR_MENU_FRAGMENT";
+    String QUICK_SETTINGS_MENU_FRAGMENT = "QUICK_SETTINGS_MENU_FRAGMENT";
+    String RECENT_MENU_FRAGMENT = "RECENT_MENU_FRAGMENT";
+    String LOCK_SCREEN_MENU_FRAGMENT = "LOCK_SCREEN_MENU_FRAGMENT";
+    String SYSTEM_MENU_FRAGMENT = "SYSTEM_MENU_FRAGMENT";
+}

src/com/pixel/extension/fragments/Lockscreen.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2019 TitaniumOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixel.extension.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
+public class Lockscreen extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    
+    private static final String TAG = "Lockscreen";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lockscreen);
+        setRetainInstance(true);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EXTENSION;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+}

src/com/pixel/extension/fragments/Navigation.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2019 TitaniumOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixel.extension.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
+public class Navigation extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    
+    private static final String TAG = "Navigation";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.navigation);
+        setRetainInstance(true);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EXTENSION;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+}

src/com/pixel/extension/fragments/Recents.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2019 TitaniumOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixel.extension.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
+public class Recents extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    
+    private static final String TAG = "Recents";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.recents);
+        setRetainInstance(true);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EXTENSION;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+}

src/com/pixel/extension/fragments/Statusbar.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2019 TitaniumOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixel.extension.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
+public class Statusbar extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    
+    private static final String TAG = "Statusbar";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.statusbar);
+        setRetainInstance(true);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EXTENSION;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+}

src/com/pixel/extension/fragments/System.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright (C) 2019 TitaniumOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixel.extension.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
+public class System extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    
+    private static final String TAG = "System";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.system);
+        setRetainInstance(true);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EXTENSION;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        return true;
+    }
+
+}

src/com/pixel/extension/views/MenuViews.java
@@ -1,0 +1,68 @@
+package com.pixel.extension.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+public class MenuViews extends LinearLayout {
+
+    TextView menu_title;
+    ImageView menu_icon;
+    String title;
+    int icon;
+
+    public MenuViews(Context context) {
+        super(context);
+        init(context, null, 0, 0);
+    }
+
+    public MenuViews(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0, 0);
+    }
+
+    public MenuViews(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr, 0);
+    }
+
+
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        View base = inflate(context, R.layout.menu_view, this);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MenuViews, defStyleAttr, defStyleRes);
+        title = typedArray.getString(R.styleable.MenuViews_title);
+        icon = typedArray.getResourceId(R.styleable.MenuViews_icon, 0);
+        menu_title = base.findViewById(R.id.menu_title);
+        menu_icon = base.findViewById(R.id.menu_icon);
+        if (!title.isEmpty()) {
+            menu_title.setText(title);
+        } else {
+            menu_title.setText("Not Available");
+        }
+        menu_icon.setImageResource(icon);
+        typedArray.recycle();
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setIcon(int image) {
+        this.icon = image;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}